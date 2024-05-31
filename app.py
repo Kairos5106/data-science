7bfcbdd3fc589948ad9916ca16438a2c3541f23a
kevinhotzewan <kevinhotzewan@gmail.com>
2024-05-31 16:16:30 +0800

    added a basic layout for the web app

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
-# Code goes here
+# Page title & header
+st.title("Phishing Website Detector")
+st.markdown("A machine learning application used to identify a malicious website from its link. Input a link to get started!")
+
+# Get user input
+user_input = st.text_input("Enter a website link here")
+st.markdown(f"Your input is: {user_input}")
+
+# Pass data to model
+
+# Produce output
+st.warning("This website is malicious!")
